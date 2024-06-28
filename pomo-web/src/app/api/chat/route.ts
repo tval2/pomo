eba503df59e40440faa6ebcd2b6facefdde4ca05
Sam Gondelman <samuel_gondelman@alumni.brown.edu>
2024-06-27 22:30:16 -0700

    refactoring, trying to get audio streaming working but it's not quite there

--- pomo-web/src/app/api/chat/route.ts
+++ pomo-web/src/app/api/chat/route.ts
@@ -4,14 +4,14 @@ import { sendMessage2LLM } from "./chat";
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
-    if (!body.imageData) {
+    if (!body.data) {
       return new Response(
-        JSON.stringify({ message: "No image data provided in chat api call" }),
+        JSON.stringify({ message: "No data provided in chat api call" }),
         { status: 400, headers: { "Content-Type": "application/json" } }
       );
     }
 
-    const streamGenerator = await sendMessage2LLM(body.imageData);
+    const streamGenerator = await sendMessage2LLM(body.data);
 
     const stream = new ReadableStream({
       async start(controller) {
